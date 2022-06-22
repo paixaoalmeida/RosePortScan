16ad4eb9e0c32db6e81f5b3c39ba78c7f7e53632
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-21 23:25:19 -0300

    Update RosePortScan.py
    Inicio v1.1

--- RosePortScanPython/RosePortScan.py
+++ RosePortScanPython/RosePortScan.py
@@ -1,11 +1,12 @@
+#GitHub: github.com/paixaoalmeida
 #Meu primeiro programa em Python, um portscan básico que é baseado numa lista de portas TCP populares
 #Comecei dia 26 e enrrolei, dia 03 de Junho dei uma pesquisado num problema e consegui arrumar o timeout etc
 #Apenas diz se a porta está aberta e se tiver, passa um banner do serviço rodando na porta
-#GitHub: github.com/paixaoalmeida
-#Todo o código está comentado pois preciso lembrar das coisas mais básicas!
 #Ultima alteração dia 03 de Junho ás 17:02
-#Alterações minímas no código: dia 19 de junho | próxima atualização será da v1.1 (toda uma atualização)
-
+#Alterações minímas no código: dia 19 de junho
+#alterações nas saídas do programa e 3 portas adicionadas no array de portas (prod v1.1) (21 de junho)
+#-----------------------------------------------------------------------------------------------------------------
+#Variáveis
 
 import socket
 import sys
@@ -13,27 +14,29 @@ import time
 
 ip = sys.argv[1]
 
-listaportas = [20, 21, 22, 23, 25, 80, 110, 139, 443, 445]  #Lista para as portas
+listaportas = [20, 21, 22, 23, 25, 80, 110, 139, 143, 194, 443, 445, 3389]
+#----------------------------------------------------------------------------------------------------------
+#Código do programa
 
-print(f"Escaneando portas no host {ip}\n")
+print(f"\033[4;31m----> Escaneando portas no host ----->\033[m {ip}\n")
 
-for portas in listaportas:  #For loop para ler a lista e fazer as conexões
-    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #biblioteca socket
-    meusocket.settimeout(5)  #Timeout para conectar nas portas (p nao ficar eternamente tentando conectar)
-    res = meusocket.connect_ex((ip, portas))  #Conetar o socket no host desejado, modo TCP, endereço e porta
-    time.sleep(2)
+for portas in listaportas:
+    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    meusocket.settimeout(5)  #Timeout para conectar nas portas
+    res = meusocket.connect_ex((ip, portas))
+    time.sleep(1)
 
-    if (res == 0):   #Se for 0 então a porta está ativa
-        print(f"Porta {portas} aberta\n")
+    if res == 0:   #Código 0, socket ativo
+        print(f"\033[1;33mPorta {portas} aberta\033[m \n")
 
         try:
-            servico = meusocket.recv(1024)   #Receber a resposta (no caso o banner da porta)
-            encodado = servico.decode("utf-8")  #Decodando a variavel de bytes para string, saida sem sujeira!
-            print(f"Serviço rodando na porta {portas} ------> {encodado} \n")
+            servico = meusocket.recv(1024)   #Receber dados da porta
+            encodado = servico.decode("utf-8")  #Decodando de bytes para utf-8
+            print(f"\033[1mServiço rodando na\033[m \033[1;33mporta {portas}\033[m ------> {encodado}\n")
         except:
-            print(f"Sem banner capturado na porta {portas} \n")
-meusocket.close()
+            print(f"\033[1mSem banner capturado na porta {portas}\033[m \n")
+    meusocket.close()
 
-print("Sem mais portas abertas encontradas!")
+print("\033[1mSem mais portas abertas encontradas!\033[m")
 
 
